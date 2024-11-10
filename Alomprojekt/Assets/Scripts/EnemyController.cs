623bf13d5d459669d673d440e0277823472ea321
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-10 21:15:36 +0100

    Script létrehozás, ütközés érzékelés, játékos karakter életerő levonás

---
+++ Alomprojekt/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+
+}
