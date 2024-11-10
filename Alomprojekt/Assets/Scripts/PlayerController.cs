471d750d0a32e67d34c66fc20817bb640cc36e28
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-11-10 21:21:23 +0100

    Alap életerő, lövés funkció létrehozása,

--- Alomprojekt/Assets/Scripts/PlayerController.cs
+++ Alomprojekt/Assets/Scripts/PlayerController.cs
@@ -8,27 +8,83 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;      // Math.Clamp() ?
 
 
-    // Start is called before the first frame update
+    // Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
+
+
+    // Variables related to projectiles
+    public GameObject projectilePrefab;
+    public InputAction launchAction;
+    public float shotTravelSpeed = 300.0f;
+
+    
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+
+        launchAction.Enable();
+        launchAction.performed += Launch;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+        
+
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 
 
-    void FixedUpdate()
+    public void ChangeHealth(int amount)
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
-        rigidbody2d.MovePosition(position);
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch(InputAction.CallbackContext context)
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(launchAction.ReadValue<Vector2>(), shotTravelSpeed);
     }
 
 }
