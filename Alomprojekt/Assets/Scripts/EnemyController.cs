b2696f52540642b2b49720c0f7a1aee7fa31640f
[Olaszi Bence] <[osziben2@gmail.com]>
2024-11-20 22:56:42 +0100

    EnemyController metódusainak származtatása Character osztályból, eventek bevezetése

--- Alomprojekt/Assets/Scripts/EnemyController.cs
+++ Alomprojekt/Assets/Scripts/EnemyController.cs
@@ -1,16 +1,45 @@
+using Assets.Scripts;
+using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : Assets.Scripts.Character
 {
+    /// <summary>
+    /// Változók
+    /// </summary>
 
-    void OnTriggerStay2D(Collider2D other)
+
+    /// <summary>
+    /// Komponenesek
+    /// </summary>
+
+
+    /// <summary>
+    /// Getterek és Setterek
+    /// </summary>
+
+
+    /// <summary>
+    /// Események
+    /// </summary>
+    public event Action<float> OnPlayerCollision; // Játékossal való ütközés
+
+
+/// <summary>
+/// Eseménykezelõ, amely akkor hívódik meg, amikor egy másik collider 2D-es triggerrel ütközik.
+/// Ellenõrzi, hogy a trigger objektum a PlayerController-t tartalmazza, és ha igen, 
+/// meghívja az OnPlayerCollision eseményt, amely a játékos sebzését kezeli.
+/// </summary>
+/// <param name="trigger">Az ütközõ collider, amely a trigger eseményt váltja ki</param>
+    void OnTriggerEnter2D(Collider2D trigger)
     {
-        PlayerController controller = other.GetComponent<PlayerController>();
-
-
-        if (controller != null)
+        //Debug.Log(player);
+        if (trigger.gameObject.TryGetComponent<PlayerController>(out var palyer))
         {
-            controller.ChangeHealth(-1);
+            //Debug.Log(BaseDMG);
+            OnPlayerCollision?.Invoke(-BaseDMG);
         }
     }
 
