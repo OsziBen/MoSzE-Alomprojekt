db945ee8f8f9422500f3fda45182f8f1e1c0bd9f
OsziBen <osziben2@gmail.com>
2024-12-09 23:26:58 +0100

    SaveLoadManager hozzáadása

---
+++ Alomprojekt/Assets/Scripts/Managers/TransientManagers/SaveLoadManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SaveLoadManager : BaseTransientManager<SaveLoadManager>
+{
+    public async Task<bool> SaveStateAsync(int level)
+    {
+        try
+        {
+            await Task.Delay(1000);  // Szimuláljuk a betöltési időt
+            Debug.Log($"The data of Level {level} has been saved.");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Error saving data: {ex.Message}");
+            return false;
+        }
+    }
+}
