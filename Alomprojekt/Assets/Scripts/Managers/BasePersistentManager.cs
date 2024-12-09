33a0fcfd66abdcc19aef004cd2edd202191ff455
OsziBen <osziben2@gmail.com>
2024-12-09 23:24:34 +0100

    Állandó Manager ősosztály létrehozása

---
+++ Alomprojekt/Assets/Scripts/Managers/BasePersistentManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BasePersistentManager<T> : MonoBehaviour where T : BasePersistentManager<T>
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+            Initialize();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    protected virtual void Initialize()
+    {
+        Debug.Log("Hello, I am " + this.name);
+    }
+
+}
