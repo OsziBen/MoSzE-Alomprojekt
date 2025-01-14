fff073a8c08a3c47079ebc9f1b6de3aecfed199f
OsziBen <osziben2@gmail.com>
2025-01-14 21:21:21 +0100

    Közös adattípus létrehozása

---
+++ Alomprojekt/Assets/Scripts/SpriteLevelData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[System.Serializable]
+public class SpriteLevelData
+{
+    [Range(1, 4)]
+    public int level;
+    public Sprite sprite;
+    public Collider2D collider;
+
+}
+
