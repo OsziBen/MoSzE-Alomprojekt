ca0d6f26d9bf1ce7d6f5bde82b1c479f20b6c4ce
OsziBen <osziben2@gmail.com>
2025-01-14 21:25:12 +0100

    Dinamikus sprite- és colliderkezelés implementálása

--- Alomprojekt/Assets/Scripts/ObstacleController.cs
+++ Alomprojekt/Assets/Scripts/ObstacleController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 using UnityEngine;
 
@@ -15,14 +16,27 @@ public class ObstacleController : MonoBehaviour
         prefabID = System.Guid.NewGuid().ToString();
     }
 
+    [Header("Sprites and Colliders")]
+    [SerializeField]
+    private List<SpriteLevelData> spriteLevelDataList;
+
+    CharacterSetupManager characterSetupManager;
+
     public string ID
     {
         get { return prefabID; }
     }
 
+    private void Awake()
+    {
+        characterSetupManager = FindObjectOfType<CharacterSetupManager>();
+        characterSetupManager.OnSetObstacleAttributes += SetObstacleAttributesByLevel;
+    }
+
     private void OnValidate()
     {
         ValidateUniqueID();
+        //ValidateUniqueSpriteLevels();
     }
 
     private void ValidateUniqueID()
@@ -30,6 +44,49 @@ public class ObstacleController : MonoBehaviour
         if (string.IsNullOrEmpty(prefabID))
         {
             Debug.LogError("Prefab ID is empty! Please generate or assign a unique ID.", this);
+        }
+    }
+
+    private void ValidateUniqueSpriteLevels()
+    {
+        HashSet<int> levelSet = new HashSet<int>();
+        foreach (var data in spriteLevelDataList)
+        {
+            if (levelSet.Contains(data.level))
+            {
+                Debug.LogError($"Duplicate level {data.level} found in LevelSpriteDataList.");
+            }
+            else
+            {
+                levelSet.Add(data.level);
+            }
+        }
+    }
+
+    void SetObstacleAttributesByLevel(int level)
+    {
+        SetCurrentObstacleSpriteByLevel(level);
+        characterSetupManager.OnSetObstacleAttributes -= SetObstacleAttributesByLevel;
+    }
+
+    void SetCurrentObstacleSpriteByLevel(int level)
+    {
+        foreach (var item in spriteLevelDataList)
+        {
+            Debug.Log(item.level);
+        }
+        var currentSpriteLevelData = spriteLevelDataList.FirstOrDefault(x => x.level == level);
+
+        if (currentSpriteLevelData != null)
+        {
+            // If item is found, update the sprite and collider
+            this.GetComponent<SpriteRenderer>().sprite = currentSpriteLevelData.sprite;
+            currentSpriteLevelData.collider.enabled = true;
+        }
+        else
+        {
+            // Handle case where no matching level is found
+            Debug.LogWarning($"No SpriteLevelData found for level {level}. Make sure the level exists in the data list.");
         }
     }
 }
