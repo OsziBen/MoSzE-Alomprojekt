a15cb7ee19fe56be7897081a580aa9cd6c1fc470
OsziBen <osziben2@gmail.com>
2025-01-16 22:34:53 +0100

    Vezérlő script létrehozása, alapműködés biztosítása

---
+++ Alomprojekt/Assets/Scripts/Boss/BossBodypartController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BossBodypartController : MonoBehaviour
+{
+
+    // TODO: eventek
+    public event Action OnBodypartPlayerCollision;
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.TryGetComponent<PlayerController>(out var player))
+        {
+            Debug.Log("OUCH!" + player);
+            OnBodypartPlayerCollision?.Invoke();
+        }
+    }
+}
