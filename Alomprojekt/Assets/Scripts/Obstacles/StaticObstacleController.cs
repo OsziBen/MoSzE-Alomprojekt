f5a377814d996d000be0e375ef1ab9d7291cd27c
Attila Szabó <szabat21@protonmail.com>
2025-01-19 13:08:12 +0000

    StaticObstacleController kommentek

--- Alomprojekt/Assets/Scripts/Obstacles/StaticObstacleController.cs
+++ Alomprojekt/Assets/Scripts/Obstacles/StaticObstacleController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +7,26 @@ public class StaticObstacleController : ObstacleController
     /// <summary>
     /// Komponensek
     /// </summary>
-    CharacterSetupManager characterSetupManager;
+    CharacterSetupManager characterSetupManager; // A karakter beállításokat kezelő manager.
 
 
     protected override void Awake()
     {
+        // Felkeressük a karakter beállításokat kezelő managert.
         characterSetupManager = FindObjectOfType<CharacterSetupManager>();
+        // Feliratkozunk a karakter beállításokat kezelő eseményre, hogy értesüljünk a változásokról.
         characterSetupManager.OnSetObstacleAttributes += SetObstacleAttributesByLevel;
     }
 
+    /// <summary>
+    /// A szint (level) alapján állítja be az obstacle tulajdonságait.
+    /// </summary>
+    /// <param name="level">Az aktuális szint</param>
     protected override void SetObstacleAttributesByLevel(int level)
     {
+        // Beállítja az obstacle jelenlegi sprite-ját a szint alapján.
         SetCurrentObstacleSpriteByLevel(level);
+        // Miután az obstacle beállításai frissítve lettek, leiratkozunk az eseményről.
         characterSetupManager.OnSetObstacleAttributes -= SetObstacleAttributesByLevel;
     }
 }
